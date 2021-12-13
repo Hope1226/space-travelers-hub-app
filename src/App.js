291301e2be65771529e4c1ec570b1dde7055351b
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-13 23:00:53 +0200

    Add path for profile page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
