ae7d327da48a6d12561e3c71075a2104ccad3976
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-12-14 22:53:17 +0500

    Fetch API and display rockets

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,26 +1,13 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { addRokets } from '../../redux/rockets/rockets';
+import { getRockets } from '../../redux/rockets/rockets';
 import Rocket from './Rocket';
-import Dragon from '../../assets/images/dragon-1.jpg';
 
 const Rockets = () => {
   const dispatch = useDispatch();
 
-  const addRockets = () => {
-    const newRokcet = {
-      id: Math.floor(Math.random() * 100),
-      name: 'Dragon-1',
-      img: Dragon,
-      info: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam nobis suscipit hic ex quae! Autem voluptatum vel quo corrupti assumenda quas obcaecati maxime, ipsum odit iusto illum alias consequatur aliquam',
-      reserved: false,
-    };
-
-    dispatch(addRokets(newRokcet));
-  };
-
   useEffect(() => {
-    addRockets();
+    dispatch(getRockets());
   }, []);
 
   const rocketsState = useSelector((state) => state.rocketReducer);
