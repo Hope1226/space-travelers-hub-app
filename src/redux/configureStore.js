1544698cd89227ffd16bc4a83dbda69696be9648
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-15 15:29:07 +0200

    Add rocketReducer state

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import MissionsReducer from './missions/missions';
+import rocketReducer from './rockets/rockets';
 
 const reducer = combineReducers({
   missions: MissionsReducer,
+  rocketReducer,
 });
 
 const store = createStore(
