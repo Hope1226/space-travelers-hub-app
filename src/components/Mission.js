02c9ff73d7741432cbabc825236374dab0d14024
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-15 10:35:13 +0200

    Add mission component

---
+++ src/components/Mission.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Mission = (props) => {
+  const { missionName, description } = props;
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{missionName}</td>
+            <td>{description}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+Mission.propTypes = {
+  missionName: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+export default Mission;
