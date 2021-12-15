e09d2ce041eac9a819a9392b5c0a51a9862e11b4
KARANGWA Ntwali Josue <kntwalijos@gmail.com>
2021-12-15 14:34:08 +0200

    Add handleJoinMission function

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { joinMission, leaveMission } from '../redux/missions/missions';
 
 const Mission = (props) => {
-  const { missionName, description } = props;
+  const {
+    id, joinStatus, missionName, description,
+  } = props;
+  const dispatch = useDispatch();
+  const joinedMission = useSelector((state) => state.missions.joinedMissions);
+
+  const handleJoinMission = () => {
+    if (joinStatus.status === false) {
+      joinStatus.status = true;
+      dispatch(joinMission({ id, missionName }));
+    }
+  };
 
   return (
     <div>
@@ -19,11 +32,19 @@ const Mission = (props) => {
           <tr>
             <td>{missionName}</td>
             <td>{description}</td>
+            {
+                joinStatus === true ? (
+                  <td><span>ACTIVE MEMBER</span></td>
+                ) : (
+                  <td><span> NOT A MEMBER</span></td>
+                )
+            }
             <td>
-              NOT A MEMBER
-            </td>
-            <td>
-              <button type="button">Join Mission</button>
+              { joinStatus === true ? (
+                <button type="button" onClick={handleLeaveMission}>Leave Mission</button>
+              ) : (
+                <button type="button" onClick={handleJoinMission}>Join Mission</button>
+              )}
             </td>
           </tr>
         </tbody>
@@ -33,6 +54,9 @@ const Mission = (props) => {
 };
 
 Mission.propTypes = {
+  id: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  joinStatus: PropTypes.object.isRequired,
   missionName: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };
