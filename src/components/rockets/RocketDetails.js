cf428798c36b3b86ba45a79dad97559a48a8e1df
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2021-12-17 01:33:13 +0500

    Style pop up

--- src/components/rockets/RocketDetails.js
+++ src/components/rockets/RocketDetails.js
@@ -8,41 +8,43 @@ const RocketDetails = ({
   const fileteredList = rocketList.filter((rocket) => rocket.id === Number(rocketId));
 
   return (
-    <div className="popup-details">
-      <button type="button" className="closeBtn" onClick={closeFunc}>X</button>
-      <div className="rocket-info">
-        <div className="pop-up-img">
-          <img src={fileteredList[0].img} alt="rocket" />
-        </div>
-        <div className="rocket-det">
-          <h2>{fileteredList[0].name}</h2>
-          <p>{fileteredList[0].description}</p>
-          <h3>Details</h3>
-          <ul>
-            <li>
-              Country of origin:
-              {fileteredList[0].origin}
-            </li>
-            <li>
-              Manufacturer:
-              {fileteredList[0].manufacturer}
-            </li>
-            <li>
-              Height (feet):
-              {fileteredList[0].height}
-            </li>
-            <li>
-              Diameter (feet):
-              {fileteredList[0].diameter}
-            </li>
-            <li>
-              Mass (lb):
-              {fileteredList[0].mass}
-            </li>
-          </ul>
-          <div className="actions">
-            <a href={fileteredList[0].wiki_link} className="wiki-link" target="_balnk">See more</a>
-            <button type="button" onClick={cancelFunc}>Cancel Reservation</button>
+    <div className="wrapper">
+      <div className="popup-details">
+        <button type="button" className="closeBtn" onClick={closeFunc}>Close</button>
+        <div className="rocket-info-pop">
+          <div className="pop-up-img">
+            <img src={fileteredList[0].img} alt="rocket" />
+          </div>
+          <div className="rocket-det">
+            <h2>{fileteredList[0].name}</h2>
+            <p>{fileteredList[0].description}</p>
+            <h3>Details</h3>
+            <ul>
+              <li>
+                <b>Country of origin:</b>
+                {fileteredList[0].origin}
+              </li>
+              <li>
+                <b>Manufacturer:</b>
+                {fileteredList[0].manufacturer}
+              </li>
+              <li>
+                <b>Height (feet):</b>
+                {fileteredList[0].height}
+              </li>
+              <li>
+                <b>Diameter (feet):</b>
+                {fileteredList[0].diameter}
+              </li>
+              <li>
+                <b>Mass (lb):</b>
+                {fileteredList[0].mass}
+              </li>
+            </ul>
+            <div className="actions">
+              <a href={fileteredList[0].wiki_link} className="wiki-link" target="_balnk">See more</a>
+              <button type="button" onClick={cancelFunc}>Cancel Reservation</button>
+            </div>
           </div>
         </div>
       </div>
